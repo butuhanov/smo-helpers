504d1ae6bc698743fc8ba502549626fc648e837d
Basil <36451784+butuhanov@users.noreply.github.com>
2020-11-18 21:53:16 +0700

    Update main.go

--- vk/main.go
+++ vk/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// using VK Callback API
 var (
 	confirmationToken   = os.Getenv("CONFIRMATION_TOKEN")
 	token               = os.Getenv("TOKEN")
